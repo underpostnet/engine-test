c486c887110f2b6281de5376394171db5226bd02
underpostnet <development@underpost.net>
2025-11-11 23:28:13 +0000

    ci(engine-test-repo-build): ⚙️ Update engine test repository

--- src/cli/index.js
+++ src/cli/index.js
@@ -162,6 +162,7 @@ program
   .option('--worker', 'Sets the context for a worker node.')
   .option('--chown', 'Sets the appropriate ownership for Kubernetes kubeconfig files.')
   .option('--k3s', 'Initializes the cluster using K3s (Lightweight Kubernetes).')
+  .option('--hosts <hosts>', 'A comma-separated list of cluster hostnames or IP addresses.')
   .action(Underpost.cluster.init)
   .description('Manages Kubernetes clusters, defaulting to Kind cluster initialization.');
 
@@ -191,6 +192,7 @@ program
   .option('--traffic <traffic-versions>', 'A comma-separated list of custom deployment traffic weights.')
   .option('--disable-update-deployment', 'Disables updates to deployments.')
   .option('--disable-update-proxy', 'Disables updates to proxies.')
+  .option('--disable-deployment-proxy', 'Disables proxies of deployments.')
   .option(
     '--status',
     'Retrieves current network traffic data from resource deployments and the host machine network configuration.',
@@ -381,6 +383,8 @@ program
   .option('--replicas <replicas>', 'Sets a custom number of replicas for deployment.')
   .option('--pod-name <pod-name>', 'Optional: Specifies the pod name for execution.')
   .option('--node-name <node-name>', 'Optional: Specifies the node name for execution.')
+  .option('--port <port>', 'Optional: Specifies the port for execution.')
+  .option('--etc-hosts', 'Enables etc-hosts context for the runner execution.')
   .option('--volume-host-path <volume-host-path>', 'Optional: Specifies the volume host path for test execution.')
   .option('--volume-mount-path <volume-mount-path>', 'Optional: Specifies the volume mount path for test execution.')
   .option('--volume-type <volume-type>', 'Optional: Specifies the volume type for test execution.')
